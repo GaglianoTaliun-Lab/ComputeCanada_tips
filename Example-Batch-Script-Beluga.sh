35ebe93ec496e49a31cc29148b8eb08d3fb8bb49
sgagliano <sgagliano@users.noreply.github.com>
2022-08-03 16:58:50 -0400

    Create Example-Batch-Script-Beluga.sh

---
+++ Example-Batch-Script-Beluga.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+#SBATCH --time=00:30:00 
+
+#SBATCH --job-name=test    
+
+#SBATCH --account=def-gsarah 
+
+#SBATCH --output=sbatch-test.log 
+
+
+echo Bonjour #replace this line with the command to run
